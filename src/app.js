98b9894c13db062c5986cf8db7b180fa39d591da
AlexH117 <alexjs415@outlook.com>
2022-11-24 17:34:16 -0600

    webBD
    No jala, revisar porque no abre la pagina

--- src/app.js
+++ src/app.js
@@ -4,10 +4,31 @@ const myconnection = require('express-myconnection');
 const mysql = require('mysql');
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const database = require('mime-db');
 
 const app = express();
 app.set('port', 4000);
 
+app.set('views', __dirname + 'views');
+app.engine('.hbs', engine({
+   extname: '.hbs',
+}));
+app.set('view engine', 'hbs');
+
+app.use(bodyParser.json);
+
+app.use(myconnection(mysql, {
+   host: 'localhost',
+   user: 'root',
+   password: '',
+   port: 3306,
+   database: '',
+}));
+
 app.listen(app.get('port'), () => {
    console.log('Listening on port ', app.get('port'));
+});
+
+app.get('/', (req, res) => {
+   res.render('home');
 });
