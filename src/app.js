0ce4fc6f41df667a7234e9863ab75c79b5f6245a
AlexH117 <alexjs415@outlook.com>
2022-11-24 18:36:18 -0600

    Conexión a localhost LISTO
    Falta conectarlo a mysql

--- src/app.js
+++ src/app.js
@@ -4,26 +4,28 @@ const myconnection = require('express-myconnection');
 const mysql = require('mysql');
 const session = require('express-session');
 const bodyParser = require('body-parser');
-const database = require('mime-db');
 
 const app = express();
 app.set('port', 4000);
 
-app.set('views', __dirname + 'views');
+app.set('views', __dirname + '/views');
 app.engine('.hbs', engine({
    extname: '.hbs',
 }));
 app.set('view engine', 'hbs');
 
-app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
+app.use(bodyParser.json());
 
-app.use(myconnection(mysql, {
+/* app.use(myconnection(mysql, {
    host: 'localhost',
    user: 'root',
    password: '',
    port: 3306,
-   database: '',
-}));
+   database: ''
+})); */
 
 app.listen(app.get('port'), () => {
    console.log('Listening on port ', app.get('port'));
