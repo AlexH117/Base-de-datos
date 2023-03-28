871ea9ee6742b3d651cbd9fbc063a94aab364763
AlexH117 <alexjs415@outlook.com>
2023-03-28 17:24:45 -0600

    Update EstructuraBD.sql

--- BDD/EstructuraBD.sql
+++ BDD/EstructuraBD.sql
@@ -1,75 +1,64 @@
 create database REGINA;
 USE REGINA;
-drop table usuario;
-
-create table rol (
-id_r int primary key not null,
-descripcion varchar(16)
-);
-
-create table usuario (
-id_usuario int primary key not null auto_increment,
-usuario varchar(24),
-contraseña varchar(16), 
-id_rol int not null, 
-constraint fk_usuario_rol foreign key (id_rol) references rol(id_r)
-);
-
-create table modulo (
-id_modulo int primary key not null auto_increment,
-descripcion varchar(16)
-);
-
-create table permiso(
-id_permiso int primary key not null auto_increment,
-descripcion varchar(16),
-id_modulo int, constraint fk_modulo foreign key(id_modulo) references modulo(id_modulo)
-);
-
-create table rol_permiso (
-id int primary key not null auto_increment,
-id_rol int, constraint fk_rol foreign key(id_rol) references usuario(id_rol),
-id_permiso int, constraint fk_permiso foreign key(id_permiso) references permiso(id_permiso)
-);
 
 create table doctor (
 id_doctor int(13) primary key not null auto_increment,
-primer_apellido nvarchar(40),
-segundo_apellido nvarchar(40),
-nombre nvarchar(40),
-edad int(8),
+primer_apellido nchar(20),
+segundo_apellido nchar(20),
+nombre nvarchar(20),
+fecha_nacim date,
 cedula nvarchar(40),
 correo nvarchar(40),
+telefono nvarchar(10),
 foto longblob
 );
-
-create table usuario_doctor (
-id_udorctor int primary key not null auto_increment,
-id_usuario int, constraint fk_udoctor foreign key(id_usuario) references usuario(id_usuario),
-id_doctor int, constraint fk_docusuario foreign key(id_doctor) references doctor(id_doctor)
-);
+drop table doctor;
 
 create table paciente (
 clave int(13) primary key not null,
-primer_apellido nvarchar(40),
-segundo_apellido nvarchar(40),
-nombre nvarchar(40),
-edad int(8),
-curp nvarchar(40),
-telefono int(16),
-foto longblob,
-id_doctor int, constraint fk_doctor foreign key (id_doctor) references doctor(id_doctor)
+primer_apellido nchar(20),
+segundo_apellido nchar(20),
+nombre nvarchar(20),
+fecha_nacim date,
+curp nchar(18),
+telefono nvarchar(10),
+id_doctor int(13),
+constraint fk_doctor foreign key (id_doctor) references doctor(id_doctor)
 );
+drop table paciente;
 
 create table expediente (
 id_expediente int primary key not null auto_increment,
 clave_paciente int(13), 
 constraint fk_paciente foreign key (clave_paciente) references paciente(clave),
+id_doctor int(13),
+constraint fk_expDoctor foreign key (id_doctor) references doctor(id_doctor),
  fecha datetime,
- motivo nvarchar(100),
- diagnostico nvarchar(100),
- tratamiento nvarchar(100),
- comentario nvarchar(150)
+ motivo nvarchar(4000),
+ diagnostico nvarchar(4000),
+ tratamiento nvarchar(4000),
+ comentario nvarchar(4000)
 );
+drop table expediente;
 
-select * from rol_permiso;
+create table adminBDD (
+	id_empleado int(10) primary key not null auto_increment,
+    primer_apellido nchar(20),
+    segundo_apellido nchar(20),
+    nombre nvarchar(20),
+    fecha_nacim date,
+    telefono nvarchar(10)
+);
+drop table adminBDD;
+
+create table perfiles (
+	roles nchar(20),
+    primary key (roles),
+    id_empleadoBDD int(10), 
+	constraint fk_perfEmpleBDD foreign key (id_empleadoBDD) references adminBDD(id_empleado),
+	id_doctores int(13),
+	constraint fk_perfDoctor foreign key (id_doctores) references doctor(id_doctor),
+    contrasenia nvarchar(10),
+    descripcion nvarchar(255)
+);
+drop table perfiles;
